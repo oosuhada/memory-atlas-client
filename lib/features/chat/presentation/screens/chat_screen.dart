437c54f64eadac4e2a54e9f5f9533b29c244afde
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-02-07 21:33:20 +0900

    feat(app): implement chat-screen screen logic

--- lib/features/chat/presentation/screens/chat_screen.dart
+++ lib/features/chat/presentation/screens/chat_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io' show Platform; // Platform import
 
-import 'package:memory_atlas_client/core/utils/dialog_utils.dart';
+import 'package:cherryrecorder_client/core/utils/dialog_utils.dart';
 import 'package:flutter/foundation.dart'; // kIsWeb, kDebugMode import
 import 'package:flutter/material.dart';
 // RawKeyboardListener 사용을 위해 추가
