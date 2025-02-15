940f7e894de788c698b9df51c284009b88bc99a0
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-02-15 09:01:37 +0900

    feat(app): implement memo.g screen logic

---
+++ lib/core/models/memo.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'memo.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MemoAdapter extends TypeAdapter<Memo> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Memo read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Memo(
+      id: fields[0] as String?,
+      placeId: fields[1] as String,
+      placeName: fields[8] as String?,
+      latitude: fields[2] as double,
+      longitude: fields[3] as double,
+      content: fields[4] as String,
+      tags: fields[5] as String?,
+      createdAt: fields[6] as DateTime?,
+      updatedAt: fields[7] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Memo obj) {
+    writer
+      ..writeByte(9)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.placeId)
+      ..writeByte(2)
+      ..write(obj.latitude)
+      ..writeByte(3)
+      ..write(obj.longitude)
+      ..writeByte(4)
+      ..write(obj.content)
+      ..writeByte(5)
+      ..write(obj.tags)
+      ..writeByte(6)
+      ..write(obj.createdAt)
+      ..writeByte(7)
+      ..write(obj.updatedAt)
+      ..writeByte(8)
+      ..write(obj.placeName);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MemoAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
